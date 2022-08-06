71b697893bd26a39c854d459f102a047a183cbad
Inusah Said <saidinusah29@gmail.com>
2022-08-06 11:57:38 +0000

    added morgan for logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+  app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms'),
+  );
   await app.listen(4920);
 }
+
 bootstrap();
